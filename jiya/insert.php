5de58e26a496a337e347e5d4653b6f221bbb2e6d
jiyahargun <jiyahargun82@gmail.com>
2025-12-01 10:24:39 +0530

    commit

--- jiya/insert.php
+++ jiya/insert.php
@@ -1,19 +1,45 @@
 <?php
 
-    include('connect.php');
+include 'connect.php';
 
-    $p_name = $_POST['p_name'];
-    $p_price = $_POST['p_price'];
-    $p_des = $_POST['p_des'];
 
-    if($p_name=="" && $p_price=="" && $p_des== "")
-    {
-        echo '0';
-    }
-    else
-    {
-        $sql = "insert into jiya_products (p_name,p_price,p_des)values('$p_name','$p_price','$p_des')";
-        mysqli_query($con,$sql);
-    }
+$upload_path = 'images/';
+
+
+
+//creating the upload url
+$upload_url = 'https://' . $_SERVER['SERVER_NAME'] . "/API/" . $upload_path;
+
+//getting name from the request
+$p_name  = $_REQUEST['p_name'];
+$p_price = $_REQUEST['p_price'];
+$p_des   = $_REQUEST['p_des'];
+
+//getting file info from the request (FIXED)
+$fileinfo = pathinfo($_FILES["p_img"]["p_name"]);
+
+//getting the file extension
+$extension = $fileinfo["extension"];
+
+//random file name
+$random = 'image_' . rand(1000,9999);
+
+//file url to store in the database
+$file_url = $upload_url . $random . '.' . $extension;
+
+//file path to upload in the server
+$file_path = $upload_path . $random . '.' . $extension;
+
+//saving the file
+move_uploaded_file($_FILES["p_img"]["p_name"], $file_path);
+
+//SQL INSERT (FIXED: jiya_products)
+$sql = "INSERT INTO jiya_products(p_name, p_price, p_des, p_img) 
+        VALUES ('$p_name', '$p_price', '$p_des', '$file_url')";
+
+$ex = mysqli_query($con, $sql);
+
+//closing the connection
+mysqli_close($con);
 
 ?>
